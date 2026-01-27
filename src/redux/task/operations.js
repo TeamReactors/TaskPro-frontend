2c9f839152be56008f0af6f56dc0df30307fd803
yesimbozkurt <yesimsbozkurt@gmail.com>
2026-01-28 00:59:35 +0300

    Update task operations

--- src/redux/task/operations.js
+++ src/redux/task/operations.js
@@ -5,49 +5,57 @@ axios.defaults.baseURL = "https://taskpro-backend-yofv.onrender.com/";
 
 export const fetchTask = createAsyncThunk(
     "tasks/fetchTasks",
-    async (_, thunkAPI) => {
+    async (taskData, thunkAPI) => {
         try {
-            const { data: res } = await axios.get("tasks");
+            const { data: res } = await axios.get(`tasks/${taskData.board_id}`);
             return res.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
         }
-    }
+    },
 );
 export const addTask = createAsyncThunk(
     "tasks/addTask",
     async (taskData, thunkAPI) => {
         try {
-            const { data: res } = await axios.post("tasks", taskData);
+            const { data: res } = await axios.post(
+                `tasks/${taskData.board_id}`,
+                taskData,
+            );
             return res.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
         }
-    }
+    },
 );
 
 export const deleteTask = createAsyncThunk(
     "tasks/deleteTask",
-    async (taskId, thunkAPI) => {
+    async (taskData, thunkAPI) => {
         try {
-            const { data: res } = await axios.delete(`tasks/${taskId}`);
+            const { data: res } = await axios.delete(
+                `tasks/${taskData.board_id}/${taskData.task_id}`,
+            );
             return res.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
         }
-    }
+    },
 );
 
 export const moveTask = createAsyncThunk(
     "tasks/moveTask",
-    async ({ taskId, newStatus }, thunkAPI) => {
+    async (taskData, thunkAPI) => {
         try {
-            const { data: res } = await axios.patch(`tasks/${taskId}`, {
-                status: newStatus
-            });
+            const { data: res } = await axios.patch(
+                `tasks/${taskData.board_id}/${taskData.task_id}/move`,
+                {
+                    column_id: taskData.column_id,
+                },
+            );
             return res.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
         }
-    }
+    },
 );
