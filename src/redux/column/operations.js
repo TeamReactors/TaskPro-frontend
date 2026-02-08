6ffd30861356f719be1d293f07f65175379e7b72
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-02-09 01:08:41 +0300

    Refactor operations: add async thunks for fetching, adding, deleting, and updating columns

--- src/redux/column/operations.js
+++ src/redux/column/operations.js
@@ -1,5 +1,52 @@
-// import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://taskpro-backend-yofv.onrender.com/";
 
+export const fetchColumns = createAsyncThunk(
+    "column/fetchColumns",
+    async (boardId, thunkAPI) => {
+        try {
+            const { data: res } = await axios.get(`column/${boardId}`);
+            return res.data;
+        }
+        catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    });
+
+export const addColumn = createAsyncThunk(
+    "column/addColumn",
+    async (columnData, thunkAPI) => {
+        try {
+
+            const { data: res } = await axios.post("column", columnData);
+            return res.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    });
+
+
+export const deleteColumn = createAsyncThunk(
+    "column/deleteColumn",
+    async (columnId, thunkAPI) => {
+        try {
+
+            const { data: res } = await axios.delete(`column/${columnId}`);
+            return res.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    });
+export const updateColumn = createAsyncThunk(
+    "column/updateColumn",
+    async (columnData, thunkAPI) => {
+        try {
+
+            const { data: res } = await axios.patch(`column/${columnData.id}`, columnData);
+            return res.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    });
