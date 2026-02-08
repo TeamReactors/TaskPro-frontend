6b725f62f918808e6d66dd0bac10292c75abe2e6
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-02-09 01:08:18 +0300

    Temporarily comment out StrictMode in main rendering

--- src/components/BoardColumn.jsx
+++ src/components/BoardColumn.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react';
-
+import { useSelector } from "react-redux";
+// Reducer Selectors
+import { selectTask } from '../redux/task/selectors';
 
 const BoardColumn = ({ data }) => {
 
-    const { tasks, column } = data;
+    const { column } = data;
     console.log(data)
+    const tasks = useSelector(selectTask);
+    console.log(column.title, "=>  ", tasks);
     return (
         <section className="flex flex-col gap-3.5" >
             <header className="board-column__header">
-                <h3 className="text-white">{data.column.title}</h3>
+                <h3 className="text-white">{column.title}</h3>
             </header>
             <div className="board-column__body">
-
+                {
+                    tasks.filter(task => task.column_id === column.id).map((task) => (
+                        <div key={task.id} className="board-column__task">
+                            <p>{task.title}</p>
+                            <br />
+                            <p>{task.description}</p>
+                        </div>
+                    ))
+                }
             </div>
         </section>
     );
