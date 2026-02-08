a2c66b95db57c90ea6c23fedf7632aec6fd8d5f6
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-02-09 01:08:32 +0300

    Refactor HomePage: integrate Redux for board fetching and clean up imports

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,23 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 // Components
-
-import Sidebar from "../components/Sidebar";
 import ScreensPage from "../components/ScreensPage";
-import Header from "../components/Header";
+
+// Reducer Operations
+import { fetchBoard } from "../redux/board/operations";
 
 const HomePage = () => {
-
+    const dispatch = useDispatch();
     const { boardID } = useParams();
 
+    useEffect(() => {
+        dispatch(fetchBoard());
+    }, [dispatch]);
+    
     return (
-        <div className="flex h-[100vh] flex-col border-2 border-solid border-gray-300">
+        <div className="flex h-[100vh] flex-col border-2  ">
             {/* <Header /> */}
             <div className="flex flex-3/12">
                 {/* <Sidebar /> */}
