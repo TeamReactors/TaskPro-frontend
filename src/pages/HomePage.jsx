277b97ef89a8abfa3676470dfa917e6b8f7bd7cd
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-02-10 01:39:05 +0300

    Refactor HomePage component: remove ScreensPage and adjust layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,26 @@
-import { useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 // Components
-import ScreensPage from "../components/ScreensPage";
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 // Reducer Operations
 import { fetchBoard } from "../redux/board/operations";
 
 const HomePage = () => {
     const dispatch = useDispatch();
-    const { boardID } = useParams();
-
     useEffect(() => {
         dispatch(fetchBoard());
     }, [dispatch]);
-    
+
     return (
-        <div className="flex h-[100vh] flex-col border-2  ">
-            {/* <Header /> */}
+        <div className="flex h-screen flex-col border-2  ">
+            <Header />
             <div className="flex flex-3/12">
-                {/* <Sidebar /> */}
-                <ScreensPage board={boardID} />
+                <Sidebar />
+                <Outlet />
             </div>
         </div>
     )
