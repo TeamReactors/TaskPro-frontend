701f32d590e00a2f3cb51c15f2d6d2e3a7f08954
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-02-10 01:38:10 +0300

    Header title added

--- src/components/HeaderDashboard.jsx
+++ src/components/HeaderDashboard.jsx
@@ -1,10 +1,13 @@
 // Icons
 import { CiFilter } from "react-icons/ci";
 
-const HeaderDashbaord = ({ board }) => {
+const HeaderDashbaord = ({ data }) => {
+    const { board } = data;
     return (
-        <div id="headerDasboard" className="flex justify-between items-center mb-2.5">
-            <h2 className="font-medium text-white text-xl font-sans">Project Office</h2>
+        <div id="headerDasboard" className="flex justify-between items-center mb-2.5 text-white">
+            <h2 className="font-medium  text-xl font-sans">{
+                board ? board.title : ""
+            }</h2>
             <div className="flex gap-2 items-center cursor-pointer w-auto">
                 <CiFilter className="text-2xl" />
                 <button className="px-3.5 py-1.5 rounded-lg bg-transparent cursor-pointer">Filter</button>
