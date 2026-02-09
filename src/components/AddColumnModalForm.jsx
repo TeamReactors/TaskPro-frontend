848aff173c30ccda04dabcb2d1760ca23af99be6
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-02-10 01:37:06 +0300

    Add column modal form functionality with Redux integration

--- src/components/AddColumnModalForm.jsx
+++ src/components/AddColumnModalForm.jsx
@@ -1,6 +1,8 @@
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-
+// Reducer Operations
+import { useDispatch } from "react-redux";
+import { addColumn } from "../redux/column/operations";
 
 // const customStyles = {
 //     overlay: {
@@ -25,57 +27,58 @@ import * as Yup from "yup";
 //     },
 // };
 
-const AddColumnModalForm = () => {
-  
+const AddColumnModalForm = ({ onClose, boardID }) => {
+  const dispatch = useDispatch();
 
   const handleSubmit = (values, actions) => {
-    console.log(values);
+    dispatch(addColumn({ ...values, board_id: String(boardID) }));
     actions.resetForm();
+    onClose();
   };
 
   const AddColumnSchema = Yup.object().shape({
-    name: Yup.string().required("Required"),
+    title: Yup.string().required("Required"),
   });
 
   return (
     <div>
-        <Formik
-          initialValues={{
-            name: "",
-          }}
-          validationSchema={AddColumnSchema}
-          onSubmit={handleSubmit}
-        >
-          <Form className="flex flex-col bg-[#151515] w-[35%] m-auto p-[40px] space-y-5 rounded-lg relative">
-            <h1 className="text-white">Add column</h1>
-            <img
-              src="../../public/x-close.svg"
-              className="absolute right-5 top-5"
-              width={18}
-              height={18}
-            //   onClick={() => setModalIsOpen(false)}
-              alt=""
-            />
-            <Field
-              className="text-white rounded-lg px-[18px] py-[14px] border-2 border-[#3C4238] "
-              name="name"
-              type="text"
-              placeholder="Title"
-            />
-            <ErrorMessage
-              name="name"
-              component="span"
-              className="text-red-500 text-sm"
-            />
-            <button
-              className="bg-[#BEDBB0] w-[100%]  py-[14px] px-[135px] rounded-lg text-sm "
-              type="submit"
-            >
-              Add
-            </button>
-          </Form>
-        </Formik>
-      
+      <Formik
+        initialValues={{
+          title: "",
+        }}
+        validationSchema={AddColumnSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form className="flex flex-col bg-[#151515] w-87.5 m-auto p-10 space-y-5 rounded-lg relative">
+          <h1 className="text-white">Add column</h1>
+          <img
+            src="../../public/x-close.svg"
+            className="absolute right-5 top-5"
+            width={18}
+            height={18}
+            onClick={onClose}
+            alt=""
+          />
+          <Field
+            className="text-white rounded-lg px-4.5 py-3.5 border-2 border-[#3C4238] "
+            name="title"
+            type="text"
+            placeholder="Title"
+          />
+          <ErrorMessage
+            name="title"
+            component="span"
+            className="text-red-500 text-sm"
+          />
+          <button
+            className="bg-[#BEDBB0] w-full py-3.5 px-33.75 rounded-lg text-sm"
+            type="submit"
+          >
+            Add
+          </button>
+        </Form>
+      </Formik>
+
     </div>
   );
 };
