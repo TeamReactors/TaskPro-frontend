89e1f40c2514a4203975d8aae5a8065832759d6f
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-02-10 01:38:56 +0300

    Fix Sidebar component width to use fractional value

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
 const Sidebar = () => {
-    return <div className="w-[250px] flex flex-col border-solid border-gray-300 bg-amber-400">Sidebar</div>;
+    return <div className="w-62.5 flex flex-col border-solid border-gray-300 bg-amber-400">Sidebar</div>;
 };
 export default Sidebar;
