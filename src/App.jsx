7da6d54906d7436d93e44e2b278e01e3bfac92d9
caglayan <caglayanustaa@gmail.com>
2026-02-09 04:33:54 +0300

    add edit need help modal form css done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import RestrictedRoute from "./components/RestrictedRoute";
+import Modal from "react-modal";
+import EditColumnModalForm from "./components/EditColumnModalForm";
+import NeedHelpForm from "./components/NeedHelpForm";
 
 const AuthPage = lazy(() => import("./pages/AuthPage"));
 const WelcomePage = lazy(() => import("./pages/WelcomePage"));
 
+Modal.setAppElement("#root");
+
 function App() {
+  
+  
   return (
     <>
+      <NeedHelpForm></NeedHelpForm>
       <Suspense>
         <Routes>
           <Route path="/welcome" element={<WelcomePage />} />
