dddccbfbb32249bd4bce3ce1653e7deb49e1f5a6
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-03-03 14:49:16 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
-import logo from "../assets/img/logo.jpg"
+// import logo from "../assets/img/logo.jpg"
+import needHelpLogo from "../assets/img/needHelpIcon.png";
 import { selectBoardItems } from "../redux/board/selectors";
 import { logout } from "../redux/auth/operations";
 import Modal from "react-modal";
 import AddBoard from "./AddBoardModal";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+// Icons
+import { GoDotFill } from "react-icons/go";
+import { TbPencil } from "react-icons/tb";
+import { LuTrash } from "react-icons/lu";
+import { IoAdd } from "react-icons/io5";
+import { FaRegCircleQuestion } from "react-icons/fa6";
 
 const customStyles = {
     overlay: {
@@ -37,41 +44,75 @@ const Sidebar = () => {
     const dispatch = useDispatch();
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
+    const location = useLocation();
 
 
+    const activeBoard = location.pathname.split("/")[2];
     const handleClick = () => {
         dispatch(logout());
     }
-  return (
-      <div className="w-62.5 flex flex-col border-solid border-gray-300 bg-[#121212]">
-          <img className="mt-5 mb-3 ml-2" src={logo} width={104} height={32} alt="logo" />
-          <p className="text-sm text-[#888888] mt-5 mb-3 ml-2">
-              My boards
-          </p>
-          <div>
-              <p>
-                  Create a new board
-              </p>
-              <button onClick={()=> setModalIsOpen(true)}>Add Board</button>
-          </div>
-          <ul>
-              {
-                  boards.map((board) => (
-                      <li key={board.id} className="text-sm text-[#888888] mt-5 mb-3 ml-2">
-                          <Link to={`${board.id}`}>
-                              {board.title}
-                            </Link>
-                          
-                      </li>
-                    ))
-              }
-          </ul>
-          <button onClick={handleClick}>Log Out</button>
+    return (
+        <div className="w-65 flex flex-col gap-10 border-solid border-gray-300 bg-[#121212]">
+            {/* <img className="mt-5 mb-3 ml-2" src={logo} width={104} height={32} alt="logo" /> */}
+            <div className="w-full flex flex-col px-6 mt-10">
+                <p className="text-sm text-[#888888] mt-5 mb-3 ">
+                    My boards
+                </p>
+                <hr className="w-full border-gray-700" />
+                <div className="w-full flex justify-between py-3.5">
+                    <p className="text-sm w-4/12 text-white">
+                        Create a new board
+                    </p>
+                    <button onClick={() => setModalIsOpen(true)} className="flex items-center gap-2 py-2 px-2.5 bg-[#BEDBB0] hover:bg-[#BEDBB0]/40 rounded-lg  text-black hover:text-white cursor-pointer ">
+                        <IoAdd className="size-5 " />
+                    </button>
+                </div>
+                <hr className="w-full border-gray-700" />
 
-          <Modal isOpen={modalIsOpen}   onRequestClose={() => setModalIsOpen(false)} style={customStyles}>
+            </div>
+            <ul className="w-full d-flex flex-col ">
+                {
+                    boards.map((board) => (
+                        console.log(activeBoard, board.id),
+                        <Link key={board.id} to={`${board.id}`}>
+                            <li className={`flex items-center justify-between text-sm px-4 py-5 ${Number(activeBoard) === board.id ? "border-r-5 border-primary text-white" : "text-[#ffffff50]"}`}>
+                                <div className="flex items-center gap-2 py-2">
+                                    <GoDotFill className="text-primary " />
+                                    {board.title}
+                                </div>
+                                {
+                                    Number(activeBoard) === board.id &&
+                                    <div className="w-auto flex gap-2 text-[#ffffff50]">
+                                        <TbPencil className="cursor-pointer size-4 hover:text-white" />
+                                        <LuTrash className="cursor-pointer size-4 hover:text-white" />
+                                    </div>
+                                }
+                            </li>
+                        </Link>
+                    ))
+                }
+            </ul>
+
+            <div className="w-full flex flex-col gap-3.5 bg-[#1F1F1F] p-5">
+                <img src={needHelpLogo} />
+
+                <p className="text-white text-sm">
+                    If you need help with <span className="text-[#BEDBB0]">
+                        TaskPro
+                    </span>, check out our support resources or reach out to our customer support team.
+                </p>
+
+                <p className="text-white text-sm">
+                    <FaRegCircleQuestion /> Need help?
+                </p>
+            </div>
+
+            <button onClick={handleClick}>Log Out</button>
+
+            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} style={customStyles}>
                 <AddBoard setModalIsOpen={setModalIsOpen} />
-          </Modal>
-    </div>
-  );
+            </Modal>
+        </div >
+    );
 };
 export default Sidebar;
